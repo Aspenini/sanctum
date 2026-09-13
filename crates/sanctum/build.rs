9d7afdd434072ba1c3ac432873c93df06ac6cd82
Aspen <aspeninifeltner@gmail.com>
2026-09-13 19:21:30 -0400

    Use Lucide icons in Sanctum

--- crates/sanctum/build.rs
+++ crates/sanctum/build.rs
@@ -1,3 +1,8 @@
+use std::{collections::HashMap, path::PathBuf};
+
 fn main() {
-    slint_build::compile("ui/sanctum.slint").expect("failed to compile Sanctum UI");
+    let libraries = HashMap::from([("lucide".to_owned(), PathBuf::from(lucide_slint::lib()))]);
+    let config = slint_build::CompilerConfiguration::new().with_library_paths(libraries);
+    slint_build::compile_with_config("ui/sanctum.slint", config)
+        .expect("failed to compile Sanctum UI");
 }
